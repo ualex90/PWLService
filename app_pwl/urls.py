5bd935aae296c9f3a7226006fbf435e6f1bda52f
u_alex90 <u_alex90@mail.ru>
2023-11-21 21:07:38 +1000

    Fixed url

--- app_pwl/urls.py
+++ app_pwl/urls.py
@@ -5,11 +5,11 @@ from app_pwl.views.course import *
 from app_pwl.views.lesson import *
 
 urlpatterns = [
-    path('', LessonListAPIView.as_view()),
-    path('<int:pk>/', LessonRetrieveAPIView.as_view()),
-    path('create/', LessonCreateAPIView.as_view()),
-    path('<int:pk>/update/', LessonUpdateAPIView.as_view()),
-    path('<int:pk>/destroy/', LessonDestroyAPIView.as_view()),
+    path('lesson/', LessonListAPIView.as_view()),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view()),
+    path('lesson/create/', LessonCreateAPIView.as_view()),
+    path('lesson/<int:pk>/update/', LessonUpdateAPIView.as_view()),
+    path('lesson/<int:pk>/destroy/', LessonDestroyAPIView.as_view()),
 ]
 
 router = routers.SimpleRouter()
