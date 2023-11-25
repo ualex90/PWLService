b2bc7a30a74301fd22ad4f77f2a8720d99acafb9
u_alex90 <u_alex90@mail.ru>
2023-11-25 23:48:03 +1000

    Add lesson_count

--- app_pwl/serializers/course.py
+++ app_pwl/serializers/course.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
+from rest_framework.fields import IntegerField
 
 from app_pwl.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = IntegerField()
+
     class Meta:
         model = Course
         fields = '__all__'
--- app_pwl/views/course.py
+++ app_pwl/views/course.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import viewsets
 
 from app_pwl.models import Course
@@ -7,3 +8,8 @@ from app_pwl.serializers.course import CourseSerializer
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(lesson_count=Count('lessons'))
+        return queryset
