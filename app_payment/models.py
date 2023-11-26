258ad9fa1cdad955470b6ad135f7025abfc9abef
u_alex90 <u_alex90@mail.ru>
2023-11-26 15:13:36 +1000

    Mod model Payment

--- app_payment/models.py
+++ app_payment/models.py
@@ -15,11 +15,11 @@ class Payment(models.Model):
         (CASH, 'Наличные'),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', related_name='payments')
     date = models.DateTimeField(auto_now_add=True, verbose_name="Дата оплаты")
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Оплаченный курс", **NULLABLE)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="Оплаченный урок", **NULLABLE)
-    payment_amount = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Сумма оплаты")
+    payment_amount = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Сумма оплаты")
     payment_method = models.CharField(max_length=8, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты")
 
     class Meta:
@@ -29,4 +29,4 @@ class Payment(models.Model):
     def __str__(self):
         purpose = f"Курс: {self.course.name}" if self.course else (f"Курс: {self.lesson.course.name}\n"
                                                                    f"Урок: {self.lesson.name}")
-        return f'{self.user}\n{self.date}\n{purpose}'
+        return f'{self.user}\n{self.date}\n{purpose}\n'
