fe535765769560cd316bef5d5b80ce0680e7b8bc
u_alex90 <u_alex90@mail.ru>
2023-11-26 13:05:41 +1000

    Add comments for lesson_count

--- app_pwl/serializers/course.py
+++ app_pwl/serializers/course.py
@@ -5,7 +5,7 @@ from app_pwl.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    lesson_count = IntegerField()
+    lesson_count = IntegerField()  # Поле определяется в queryset во view
 
     class Meta:
         model = Course
--- app_pwl/views/course.py
+++ app_pwl/views/course.py
@@ -10,6 +10,9 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
 
     def get_queryset(self):
+        # добавление поля lesson_count в queryset
+        # путем подсчета объектов привязанных по ForeignKey
+        # по related_name поля course модели Lesson
         queryset = super().get_queryset()
         queryset = queryset.annotate(lesson_count=Count('lessons'))
         return queryset
