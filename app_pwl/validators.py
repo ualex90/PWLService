338f6e2016f0364d8ba64fe0958308810112b8b9
u_alex90 <u_alex90@mail.ru>
2023-12-03 00:32:40 +1000

    Mod validators

--- app_pwl/validators.py
+++ app_pwl/validators.py
@@ -17,7 +17,7 @@ class LinkValidator:
         val_dict = {field: dict(value).get(field) for field in self.fields}
         reg = re.compile('^(https?:\/\/)?'  # протокол
                          '([\w-]{1,32}\.[\w-]{1,32})'  # домен
-                         '([^\s@]*)'  # любой не пробельный символ + @ для исключения email-ов
+                         '([^\s@]*)'  # любой не пробельный символ + @ для исключения из проверки email адресов
                          '$')
         for field, value in val_dict.items():
             if value:
