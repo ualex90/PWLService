a9d1e370a5ec758febcfbc38fbc2d08fc5e7ccc3
u_alex90 <u_alex90@mail.ru>
2023-12-03 00:44:35 +1000

    Mod urls app_subscriptions

--- app_subscriptions/urls.py
+++ app_subscriptions/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from app_pwl.apps import AppPwlConfig
+from app_subscriptions.apps import AppSubscriptionsConfig
 from app_subscriptions.views.course import SubscribeCourseAPIView
 
-app_name = AppPwlConfig.name
+app_name = AppSubscriptionsConfig.name
 
 urlpatterns = [
     path('course/<int:pk>', SubscribeCourseAPIView.as_view(), name="subscribe_course"),
