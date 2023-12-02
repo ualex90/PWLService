b178874339891bb4d1db136aa2b383cbc5b7c35c
u_alex90 <u_alex90@mail.ru>
2023-12-02 19:09:39 +1000

    Mod tests

--- app_pwl/tests.py
+++ app_pwl/tests.py
@@ -40,7 +40,10 @@ class LessonTest(APITestCase):
         )
 
     def test_lesson_create(self):
-        """Тестирование создания урока"""
+        """
+        Тестирование создания урока
+        с минимальным заполнением полей
+        """
 
         # Принудительно аутентифицируем пользователя
         self.client.force_authenticate(user=self.user)
@@ -71,75 +74,22 @@ class LessonTest(APITestCase):
             lesson_count + 1
         )
 
-    def test_lesson_video_link_validation(self):
-        """Тестирование валидации при создании урока с допустимой ссылкой"""
+    def test_lesson_materials_validation(self):
+        """
+        Тестирование валидации при создании урока,
+        с допустимой ссылкой в текстовом поле модели
+        """
 
         # Принудительно аутентифицируем пользователя
         self.client.force_authenticate(user=self.user)
 
-        link_valid = [
+        # Текст ограничен 50 символами (согласно максимальной длины для поля "name")
+        text_valid_list = [
             "https://www.youtube.com/watch?v=qweasdzxc",
             "www.youtube.com/watch?v=qweasdzxc",
             "youtube.com/watch?v=qweasdzxc",
             "https://youtu.be/qweasdzxc",
             "youtu.be/mHQXz5FctRg",
-        ]
-
-        for link in link_valid:
-            response = self.client.post(
-                reverse("app_pwl:lesson_create"),
-                data={
-                    "name": "Test Lesson",
-                    "description": "Description Test Lesson",
-                    "course": self.course.id,
-                    "video": link
-                }
-            )
-
-            self.assertEquals(
-                response.status_code,
-                status.HTTP_201_CREATED
-            )
-
-    def test_lesson_video_link_validation_error(self):
-        """Тестирование валидации при создании урока с недопустимой ссылкой"""
-
-        # Принудительно аутентифицируем пользователя
-        self.client.force_authenticate(user=self.user)
-
-        link_invalid = [
-            "https://dzen.ru/video/watch/qweasdzxc",
-            "dzen.ru/video/watch/qweqweqwe",
-            "https://vk.com/video-123123123_123123123",
-            "vk.com/video-123123123_123123123",
-        ]
-
-        for link in link_invalid:
-            response = self.client.post(
-                reverse("app_pwl:lesson_create"),
-                data={
-                    "name": "Test Lesson",
-                    "description": "Description Test Lesson",
-                    "course": self.course.id,
-                    "video": link
-                }
-            )
-
-            self.assertEquals(
-                response.status_code,
-                status.HTTP_400_BAD_REQUEST
-            )
-
-    def test_lesson_materials_validation(self):
-        """
-        Тестирование валидации при создании тела урока,
-        названия и описания с допустимой ссылкой
-        """
-
-        # Принудительно аутентифицируем пользователя
-        self.client.force_authenticate(user=self.user)
-
-        text_valid_list = [
             "Тест https://www.youtube.com/watch?v=qweasdzxc Ok",
             "Проверка www.youtube.com/watch?v=qweasdzxc link",
             "Проверка youtube.com/watch?v=qweasdzxc link",
@@ -181,14 +131,19 @@ class LessonTest(APITestCase):
 
     def test_lesson_materials_validation_error(self):
         """
-        Тестирование валидации при создании тела урока,
-        названия и описания с не допустимой ссылкой
+        Тестирование валидации при создании урока,
+        с НЕдопустимой ссылкой в текстовом поле модели
         """
 
         # Принудительно аутентифицируем пользователя
         self.client.force_authenticate(user=self.user)
 
+        # Текст ограничен 50 символами (согласно максимальной длины для поля "name")
         text_invalid_list = [
+            "https://dzen.ru/video/watch/qweasdzxc",
+            "dzen.ru/video/watch/qweqweqwe",
+            "https://vk.com/video-123123123_123123123",# Текст ограничен 50 символами (согласно максимальной длины для поля "name")
+            "vk.com/video-123123123_123123123",
             "Text https://dzen.ru/video/watch/qweasdzxc z. q/w",
             "text dzen.ru/video/watch/qweqweqwe ru, qwe? yu/yu",
             "vid https://vk.com/video-123123123_123123123 go,",
