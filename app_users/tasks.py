d0d5b60af55396101e95516982d541f0858c0763
u_alex90 <u_alex90@mail.ru>
2023-12-09 22:57:57 +1000

    Fixed user_login_per_month

--- app_users/tasks.py
+++ app_users/tasks.py
@@ -5,9 +5,9 @@ from app_users.models import User
 
 def user_login_per_month():
     now = datetime.now()
-    invalid_date = now + timedelta(days=30)
+    invalid_date = now - timedelta(days=30)
     print(invalid_date)
-    users_invalid = User.objects.filter(last_login__gt=invalid_date)
+    users_invalid = User.objects.filter(last_login__lt=invalid_date)
     print(users_invalid)
     for user in users_invalid:
         user.is_active = False
